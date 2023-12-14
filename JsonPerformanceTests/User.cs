7a6ae39499a9221b91d7bdf533251edaf772f980
daves2009 <daves2009@126.com>
2023-12-14 16:06:35 +0800

    Change the property LastName's accessibility from private to public

--- JsonPerformanceTests/User.cs
+++ JsonPerformanceTests/User.cs
@@ -4,7 +4,7 @@ public class User(Guid id, string firstName, string lastName, string fullName, s
 {
     public Guid Id { get; set; } = id;
     public string FirstName { get; set; } = firstName;
-    private string LastName { get; set; } = lastName;
+    public string LastName { get; set; } = lastName;
     public string FullName { get; set; } = fullName;
     public string Username { get; set; } = username;
     public string Email { get; set; } = email;
